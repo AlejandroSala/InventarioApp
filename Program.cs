613be195b8c63ffd30c7bd82c9ba190359786c79
Alejandro Salazar <alexsalazar8672@gmail.com>
2026-07-19 23:07:31 -0600

    Nueva carpeta models y actualizacion en Program.cs

--- Program.cs
+++ Program.cs
@@ -17,8 +17,16 @@ Console.WriteLine($"Plataforma: {Environment.OSVersion}");
 Console.WriteLine($".NET Version: {Environment.Version}");
 Console.WriteLine();
 Console.WriteLine($"Estructura del proyecto:");
+Console.WriteLine(" InvenarioApp/");
+Console.WriteLine("     |-- Program.cs");
+Console.WriteLine("     |-- InventarioApp.csproj");
+Console.WriteLine("     |-- gitignore");
+Console.WriteLine("     |-- README.md");
+Console.WriteLine("     |-- src/");
+Console.WriteLine("         |-- Models/ (Proxima clase)");
+
 Console.WriteLine("Configuración .csproj");
 Console.WriteLine("Carpeta src/ creada");
 Console.WriteLine("Metadatos configurados");
 Console.WriteLine();
-Console.WriteLine("Proximo paso: Agregar argumentos CLI y configuración de repositorio en Github");
+Console.WriteLine("Proximo paso: Checkpoint");
